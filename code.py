47ea9e4eae2b27c9c8c591afa858b6baa8bf3f45
BlueMaxMan <119946985+BlueMaxMan@users.noreply.github.com>
2023-03-12 07:57:39 +0000

    Sounds

--- code.py
+++ code.py
@@ -11,3 +11,16 @@ pygame.display.set_caption('mak gam')
 screen_width=600; screen_height=400
 global WINDOW_SIZE
 WINDOW_SIZE = (600,400)
+
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32) 
+
+
+CHUNK_SIZE = 15 ; font = "C:/WINDOWS/FONTS/TEMPSITC.ttf"
+
+white=(255, 255, 255); black=(0, 0, 0); gray=(50, 50, 50); red=(255, 0, 0); green=(0, 255, 0); blue=(0, 0, 255); yellow=(255, 255, 0)
+
+#Inventory
+global inventory_size
+inventory_size = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
